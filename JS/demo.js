89a3ce162ffc58a04ed9000c4917f19dc383f8ce
yogeshnile <yogeshnile786@gmail.com>
2020-07-28 10:57:34 +0530

    two color add in js

--- JS/demo.js
+++ JS/demo.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#a54242',
-    lineColor: '#a54242'
+  var today = new Date()
+  var hour = today.getHours();
+  if (parseInt(hour) % 2 == 1){
+    particleground(document.getElementById('particles'), {
+      dotColor: '#a54242',
+      lineColor:'#a54242'
   });
+  }
+  else{
+    particleground(document.getElementById('particles'), {
+      dotColor: '#5b6cbd',
+      lineColor:'#5b6cbd'
+  });
+  }
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
-
-
